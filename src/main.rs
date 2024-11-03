75929db44104fca3800409f6316ee1a4c215c5ad
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2024-11-03 23:21:50 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("rwc requires at least one file as an argument");
+        process::exit(1);
+    }
+
+    let file_path: &Path = Path::new(&args[1]);
+
+    if !file_path.exists() {
+        eprintln!("{} does not exist", file_path.display());
+        process::exit(1);
+    }
+
+    if !file_path.is_file() {
+        eprintln!("{} is not a file", file_path.display());
+        process::exit(1);
+    }
+
+    let file_contents: String = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+
+    let mut line_count = 0;
+    let mut word_count = 0;
+    let mut in_word = false;
+
+    for a_byte in file_contents.bytes() {
+        if a_byte == b'\n' {
+            line_count += 1;
+        }
+
+        if !(vec![9, 10, 12, 13, 32].contains(&a_byte)) {
+            in_word = false;
+        } else if !in_word {
+            in_word = true;
+            word_count += 1;
+        }
+    }
+
+    println!("Line count: {}", line_count);
+    println!("Word count: {}", word_count);
+    println!("Byte count: {}", file_contents.len());
+}
